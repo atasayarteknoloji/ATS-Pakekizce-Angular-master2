6da7a611593ca7997c4131aa43bd4fa8476be046
busra.uyar <busra.uyar@logo.com.tr>
2020-02-15 18:13:18 +0300

    Order page fixed.

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,11 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { NeworderService } from './getNewOrder/new-order.service';
-import { NewOrder } from './getNewOrder/new-order';
-import { DatePipe } from '@angular/common';
-import { UncomplateOrderService } from './getInprogressOrder/uncomplate-order.service';
-import { UncomplateOrder } from './getInprogressOrder/uncomplate-order';
-import { NeworderDetailService } from './getNewOrderDetail/new-order-detail.service';
-import { NewOrderDetail } from './getNewOrderDetail/new-order-detail';
+import {Component, OnInit} from '@angular/core';
+import {NeworderService} from './getNewOrder/new-order.service';
+import {NewOrder} from './getNewOrder/new-order';
+import {NeworderDetailService} from './getNewOrderDetail/new-order-detail.service';
+import {NewOrderDetail} from './getNewOrderDetail/new-order-detail';
 
 @Component({
   selector: 'app-order',
@@ -13,42 +10,62 @@ import { NewOrderDetail } from './getNewOrderDetail/new-order-detail';
   styleUrls: ['./order.component.scss']
 })
 export class OrderComponent implements OnInit {
- 
-  constructor(private newOrderService: NeworderService,
-    private newOrderDetailService: NeworderDetailService) { }
+
+
+  isNewOrder = true;
+  isProgressOrder = false;
+  isCompletedOrder = false;
+
   newOrder: NewOrder[]
   newOrderDetail: NewOrderDetail[];
-  viewModel='toggleNew';
-
- 
+  viewModel = 'toggleNew';
   isShow = false;
   deneme = true;
-  deneme1=true;
-  deneme2=true;
+  deneme1 = true;
+  deneme2 = true;
+
+  constructor(private newOrderService: NeworderService,
+              private newOrderDetailService: NeworderDetailService) {
+  }
+
   ngOnInit() {
   }
+
   toggleNew() {
     this.isShow = false;
-    this.deneme=true;
-    this.deneme1=true;;
+    this.deneme = true;
+    this.deneme1 = true;
+    this.isNewOrder = true;
+    this.isProgressOrder = false;
+    this.isCompletedOrder = false;
   }
+
   toggleInprogress() {
     this.deneme = false
-    this.isShow=true;
-    this.deneme1=true;
+    this.isShow = true;
+    this.deneme1 = true;
+    this.isProgressOrder = true;
+    this.isNewOrder = false;
+    this.isCompletedOrder = false;
   }
-  toggleComplatedOrder(){
-    this.deneme1 =false;
-    this.isShow=true;
-    this.deneme=true;
+
+  toggleComplatedOrder() {
+    this.deneme1 = false;
+    this.isShow = true;
+    this.deneme = true;
+    this.isCompletedOrder = true;
+    this.isProgressOrder = false;
+    this.isNewOrder = false;
   }
-  toggledyehouse(){
-    this.deneme2 =false;
-    this.isShow=true;
-    this.deneme=true;
-    this.deneme1=true
+
+  toggledyehouse() {
+    this.deneme2 = false;
+    this.isShow = true;
+    this.deneme = true;
+    this.deneme1 = true;
   }
-  print(){
+
+  print() {
     window.print();
   }
 
